7f2de2bfe7342267ad8bff654e659af43dda8fd8
Povolotskii Artem <artpovol@gmail.com>
2022-05-18 18:15:29 +0300

    add new script to index

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
+let clickButton = document.querySelector('.click-button');
 themeButton.onclick = function () {
     page.classList.toggle('dark-theme');
+};
+clickButton.onclick = function () {
+    let str = clickButton.textContent;
+    clickButton.textContent = parseInt(str.match(/\d+/)) + 1 + ' нажатий';
 };
